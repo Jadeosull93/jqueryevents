8be9becbb5e96c6ab82a9fbab1453db4d1d5e666
Jadeosull93 <jadeosullivan@hotmail.com>
2020-05-21 12:20:52 +0000

    memory game working

--- script.js
+++ script.js
@@ -1,32 +1,57 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     // first click
     hasFlippedCard = true;
     firstCard = this;
-  } else {
+  } return {
     // second click
     hasFlippedCard = false;
     secondCard = this;
 
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-      // it's a match!
-      firstCard.removeEventListener('click', flipCard);
-      secondCard.removeEventListener('click', flipCard);
-    } else {
-      // not a match
-      setTimeout(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-      }, 1500);
+    
     }
   }
+
+
+function checkForMatch () {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework
+    
+    isMatch ? disableCards() : unFlipCards();
+     
 }
 
+function disableCards (){
+      firstCard.removeEventListener('click', flipCard);
+      secondCard.removeEventListener('click', flipCard);
+
+      resetBoard();
+}
+
+function unFlipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+
+        resetBoard();
+      }, 1500);
+
+      lockBoard = false;
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null]
+}
 cards.forEach(card => card.addEventListener('click', flipCard));
